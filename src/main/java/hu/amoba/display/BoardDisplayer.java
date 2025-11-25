192aa8b140498da98123231bd281c78876f2a38e
VDarius-NYE <darovarga@gmail.com>
2025-11-25 11:15:11 +0100

    modified functions

--- src/main/java/hu/amoba/display/BoardDisplayer.java
+++ src/main/java/hu/amoba/display/BoardDisplayer.java
@@ -6,6 +6,7 @@ import hu.amoba.model.Position;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class BoardDisplayer {
     private static final Logger LOGGER = LoggerFactory.getLogger(BoardDisplayer.class);
 
